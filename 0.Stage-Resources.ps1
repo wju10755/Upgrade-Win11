3abd05df16fa79831a957025d930f8f5d83759c3
MITS <Billu@advancestuff.com>
2024-01-14 22:55:50 -0500

    Updated

--- 0.Stage-Resources.ps1
+++ 0.Stage-Resources.ps1
@@ -2,19 +2,23 @@ Write-Host -ForegroundColor Yellow "Staging script requirements..." -NoNewline
 
 $Hardware = "https://raw.githubusercontent.com/wju10755/Upgrade-Win11/main/1.HardwareReadiness.ps1"
 
-$Toast1 = "https://github.com/wju10755/Upgrade-Win11/blob/main/2.Win11_Toast1_Notif.ps1"
+$Toast1 = "https://raw.githubusercontent.com/wju10755/Upgrade-Win11/main/2.Win11_Toast1_Notify.ps1"
 
-$Download = "https://github.com/wju10755/Upgrade-Win11/blob/main/3.Windows11_Download.ps1"
+$Download = "https://raw.githubusercontent.com/wju10755/Upgrade-Win11/main/3.Windows11_Download.ps1"
 
-$Install =  "https://github.com/wju10755/Upgrade-Win11/blob/main/4.Windows11_Install.ps1"
+$Install =  "https://raw.githubusercontent.com/wju10755/Upgrade-Win11/main/4.Windows11_Install.ps1"
 
-$Toast2 = "https://github.com/wju10755/Upgrade-Win11/blob/main/5.Win11_Toast2_Notif.ps1"
+$Toast2 = "https://raw.githubusercontent.com/wju10755/Upgrade-Win11/main/5.Win11_Toast2_Notif.ps1"
 
 $Dir = "c:\install"
 
 $Capable = "C:\install\Capable.txt"
 
 $ISO = "C:\temp\Win11_23H2-x64v2.iso"
+
+if(!(Test-Path $Dir)) {
+mkdir "c:\install" *> $null
+}
 
 Set-Location "C:\install"
 
@@ -26,7 +30,7 @@ Invoke-WebRequest $Download -OutFile "$Dir\3.Windows11_Download.ps1"
 
 Invoke-WebRequest $Install -OutFile "$Dir\4.Windows11_Install.ps1"
 
-Invoke-WebRequest $Toast2 -OutFile "$Dir\4.Win11_Toast2_Notify.ps1"
+Invoke-WebRequest $Toast2 -OutFile "$Dir\5.Win11_Toast2_Notify.ps1"
 
 Write-Host -ForegroundColor Green " done."
 
@@ -46,6 +50,5 @@ try {
 catch {
     Write-Error "An error occurred: $_"
 }
-
 
 
