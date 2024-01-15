6da4e2132cb9347bde0bd2a9cae1d056a7e69d49
MITS <Billu@advancestuff.com>
2024-01-14 22:45:05 -0500

    Updated Windows 11 download link

--- 3.Windows11_Download.ps1
+++ 3.Windows11_Download.ps1
@@ -4,7 +4,7 @@ If (Test-Path -Path $Capable) {
     #Download .iso
     Start-Job -Name WebReq -ScriptBlock { 
         $ProgressPreference = 'SilentlyContinue'
-        $URL = "https://azurestorageblob.com/Win11_23H2-x64v2.iso"
+        $URL = "https://axcientrestore.blob.core.windows.net/win11/Win11_23H2-x64v2.iso"
         $Path = "C:\Install\Win11_23H2-x64v2.iso"
         Invoke-WebRequest -Uri $URL -OutFile $Path 
     }
